34f2424abf5811d01591bd55c99d3078157869f1
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-08-31 16:06:12 -0700

    fix: move out all the source specific items and just build data frame
    takes a list of data sources that have been processed by the rename_ functions, creates a data frame for following functions.

--- R/create_data_frame.R
+++ R/create_data_frame.R
@@ -3,7 +3,6 @@
 #' data sources
 #'
 #' @param list of data sets
-#' @param 
 #'
 #' @return A data frame 
 #'
@@ -15,108 +14,20 @@ create_data_frame <- function(data_list){
 	all_data = NA
 	for (a in 1:length(data_list)){
 
-		if ("HOOKID" %in% colnames(data_list[[a]]) ){
+		tmp  <- data.frame(Year = data_list[[a]]$Year,
+							Lat = data_list[[a]]$Lat,
+							Lon = data_list[[a]]$Lon,
+							State  = data_list[[a]]$State,
+							Areas  = data_list[[a]]$Areas,
+							Depth  = data_list[[a]]$Depth,
+							Sex    = data_list[[a]]$Sex,
+							Length = data_list[[a]]$Length,
+							Weight = data_list[[a]]$Weight,
+							Age    = data_list[[a]]$Age,
+							Data_Type = data_list[[a]]$Data_Type,
+							Source = data_list[[a]]$Source)
 
-			state <- ifelse( data_list[[a]]$LATDD < 42, "CA", 
-				     ifelse( data_list[[a]]$LATDD < 46 & data_list[[a]]$LATDD >= 42, "OR",
-				     ifelse( data_list[[a]]$LATDD >= 46, "WA", "OTHER")))
-
-			
-			areas <- ifelse( data_list[[a]]$SITENAME >= 500, "CCA", "non_CCA")
-
-			hkl    <- data.frame(Year = data_list[[a]]$YEAR,
-								 Lat = data_list[[a]]$LATDD,
-								 Lon = data_list[[a]]$LONDD,
-								 State  = state,
-								 Areas  = areas,
-								 Depth  = data_list[[a]]$DEPTHM,
-								 Sex    = data_list[[a]]$SEX,
-								 Length = data_list[[a]]$LENGTH,
-								 Weight = data_list[[a]]$WEIGHT,
-								 Age    = NA,
-								 Source = "nwfsc_hkl")	
-
-			all_data = rbind(all_data, hkl)
-		}
-
-		# Grab the Survey data
-		if ("Project" %in% colnames(data_list[[a]]) ){
-
-			state <- ifelse(data_list[[a]]$Latitude_dd < 42, "CA", 
-						    ifelse( data_list[[a]]$Latitude_dd < 46 & data_list[[a]]$Latitude_dd >= 42, "OR",
-						    ifelse( data_list[[a]]$Latitude_dd >= 46, "WA", "OTHER")))
-			
-			areas = NA 
-
-			survey <- data.frame(Year = data_list[[a]]$Year,
-								Lat = data_list[[a]]$Latitude_dd,
-								Lon = data_list[[a]]$Longitude_dd,
-								State  = state,
-								Areas  = areas,
-								Depth  = data_list[[a]]$Depth_m,
-								Sex    = data_list[[a]]$Sex,
-								Length = data_list[[a]]$Length_cm,
-								Weight = data_list[[a]]$Weight,
-								Age    = data_list[[a]]$Age,
-								Source = unique(data_list[[a]]$Project))	
-
-			all_data = rbind(all_data, survey)
-		}
-
-
-		# Grab the PACFIN data
-		if ("CLUSTER_NO" %in% colnames(data_list[[a]]) ){
-
-			state <- ifelse( data_list[[a]]$SOURCE_AGID == "C", "CA", 
-				     ifelse( data_list[[a]]$SOURCE_AGID == "O", "OR",
-				     ifelse( data_list[[a]]$SOURCE_AGID == "W", "WA", "OTHER")))
-			
-			 
-			areas = data_list[[a]]$PORT 
-
-			comm  <- data.frame(Year = data_list[[a]]$SAMPLE_YEAR,
-								Lat  = NA,
-								Lon  = NA,
-								State  = state,
-								Areas  = areas,
-								Depth  = NA,
-								Sex    = data_list[[a]]$SEX,
-								Length = data_list[[a]]$FISH_LENGTH / 10,
-								Weight = data_list[[a]]$FISH_WEIGHT,
-								Age    = data_list[[a]]$age1,
-								Source = "pacfin")
-
-			all_data = rbind(all_data, comm)	
-		}
-
-		# Grab the RECFIN data
-		if ("RECFIN_YEAR" %in% colnames(data_list[[a]]) ){
-
-			state <- ifelse( data_list[[a]]$STATE_NAME == "CALIFORNIA", "CA", 
-				     ifelse( data_list[[a]]$STATE_NAME == "OREGON", "OR",
-				     ifelse( data_list[[a]]$STATE_NAME == "WASHINGTON", "WA", "OTHER")))
-
-			sex <- ifelse(data_list[[a]]$FISH_SEX == "F" | data_list[[a]]$FISH_SEX == 2, "F",
-				   ifelse(data_list[[a]]$FISH_SEX == "M" | data_list[[a]]$FISH_SEX == 1, "M",
-				   		  "U"))
-			
-			 
-			areas = data_list[[a]]$RECFIN_PORT_NAME 
-
-			rec  <- data.frame(Year = data_list[[a]]$RECFIN_YEAR,
-								Lat = NA,
-								Lon = NA,
-								State  = state,
-								Areas  = areas,
-								Depth  = NA,
-								Sex    = sex,
-								Length = data_list[[a]]$RECFIN_LENGTH_MM / 10,
-								Weight = data_list[[a]]$AGENCY_WEIGHT,
-								Age    = NA,
-								Source = "recfin")
-
-			all_data = rbind(all_data, rec)	
-		}
+		all_data = rbind(all_data, tmp)			
 	}
 
 	all_data = all_data[!is.na(all_data$Year), ]
