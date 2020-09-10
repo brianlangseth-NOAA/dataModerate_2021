218d04c53393f0675c68257d2dba3163bf96fc71
Brian.Langseth <1457805086@mil>
2020-09-10 10:12:45 -0700

    feat: added numbers by state
    Numbers for length, weight, and ages by state added to output

--- R/summarized_data.R
+++ R/summarized_data.R
@@ -1,5 +1,5 @@
 #' Summarize all the data to evaluate the samples by source,
-#' year, and biological measurement (length, weight, age)
+#' year, state, and biological measurement (length, weight, age)
 #'
 #' @param dir directory location to save output
 #' @param data data frame created by the create_data_frame function
@@ -36,6 +36,29 @@ summarize_data <- function(dir = NULL, data){
 	names(data_sum) = sort(unique(data$Source))
 	summary_list$BySource = data_sum
 
+	
+	place = 1
+	data_sum <- list()
+	for(t in sort(unique(data$State))){
+	  yrs = sort(unique(data[data$State == t, "Year"]))
+	  mat = matrix(NA, length(yrs), 3)
+	  rownames(mat) = yrs
+	  colnames(mat) = c("Length", 'Weight', 'Age')
+	  ind = 1
+	  for( y in yrs){
+	    mat[ind,] = c(sum(!is.na(data[data$State == t & data$Year == y, "Length"])), 
+	                  sum(!is.na(data[data$State == t & data$Year == y, "Weight"])),
+	                  sum(!is.na(data[data$State == t & data$Year == y, "Age"])) )	
+	    ind = ind + 1
+	  }
+	  data_sum[[place]] = mat
+	  place = place + 1
+	}
+	names(data_sum) = sort(unique(data$State))
+	summary_list$ByState = data_sum
+	
+	
+	
 	if(!is.null(dir)){ 
 		cat(capture.output(print(summary_list), file = file.path(dir, "data_summary.txt"))) }
 
