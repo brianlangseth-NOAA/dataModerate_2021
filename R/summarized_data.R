7db17cbc852e43d2186eee5bd3e39ecd0db945fd
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-09-24 12:12:43 -0700

    fix: modify to output a matrix by year and source/state

--- R/summarized_data.R
+++ R/summarized_data.R
@@ -61,54 +61,58 @@ summarize_data <- function(dir = NULL, data){
 
 	place = 1; nm = NULL
 	data_sum <- list()
-	for(t in sort(unique(data$Source))){
-	  for(a in sort(unique(data$State))){
-	  	yrs = sort(unique(data[data$Source == t & data$State == a, "Year"]))
-	  	if (length(yrs) > 0){
-	  		mat = matrix(NA, length(yrs), 3)
-	  		rownames(mat) = yrs
-	  		colnames(mat) = c("Length", 'Weight', 'Age')
+	for(a in sort(unique(data$State))){
+		tmp = 0
+		yrs = sort(unique(data[data$State == a, "Year"]))
+		mat_len = mat_age = matrix(NA, length(yrs), length(unique(data$Source)))
+	  	rownames(mat_len) = rownames(mat_age) = yrs
+	  	colnames(mat_len) = colnames(mat_age) = sort(unique(data$Source))
+		for(t in sort(unique(data$Source))){
+			tmp = tmp + 1
 	  		ind = 1
 	  		for( y in yrs){
 	  		  get = which(data$Source == t & data$State == a & data$Year == y)
-	  		  mat[ind,] = c(sum(!is.na(data[get, "Length"])), 
-	  		                sum(!is.na(data[get, "Weight"])),
-	  		                sum(!is.na(data[get, "Age"])) )	
+	  		  if(length(get) > 0){
+	  		  	mat_len[ind, tmp] = sum(!is.na(data[get, "Length"])) 	
+	  		  	mat_age[ind, tmp] = sum(!is.na(data[get, "Age"]))
+	  		  }
 	  		  ind = ind + 1
 	  		}
-	  		data_sum[[place]] = mat
-	  		place = place + 1
-	  		nm = c(nm, paste0(t, "_", a))
 	  	}
-	  }
+	  	data_sum[[place]] = mat_len
+	  	data_sum[[place + 1]] = mat_age
+	  	place = place + 2
+	  	nm = c(nm,  c(paste0(a, "_length"), paste0(a, "_age")))
 	}
-	names(data_sum) = nm #paste0(sort(unique(data$Source)),"_", sort(unique(data$State)))
+	names(data_sum) = nm 
 	summary_list$BySource_ByState = data_sum	
 
 	place = 1; nm = NULL
 	data_sum <- list()
-	for(t in sort(unique(data$Source))){
-	  for(a in sort(unique(data$State_Areas))){
-	  	yrs = sort(unique(data[data$Source == t & data$State_Areas == a, "Year"]))
-	  	if (length(yrs) > 0){
-	  		mat = matrix(NA, length(yrs), 3)
-	  		rownames(mat) = yrs
-	  		colnames(mat) = c("Length", 'Weight', 'Age')
+	for(a in sort(unique(data$State_Areas))){
+		tmp = 0
+		yrs = sort(unique(data[data$State_Areas == a, "Year"]))
+		mat_len = mat_age = matrix(NA, length(yrs), length(unique(data$Source)))
+	  	rownames(mat_len) = rownames(mat_age) = yrs
+	  	colnames(mat_len) = colnames(mat_age) = sort(unique(data$Source))
+		for(t in sort(unique(data$Source))){
+			tmp = tmp + 1
 	  		ind = 1
 	  		for( y in yrs){
 	  		  get = which(data$Source == t & data$State_Areas == a & data$Year == y)
-	  		  mat[ind,] = c(sum(!is.na(data[get, "Length"])), 
-	  		                sum(!is.na(data[get, "Weight"])),
-	  		                sum(!is.na(data[get, "Age"])) )	
+	  		  if(length(get) > 0){
+	  		  	mat_len[ind, tmp] = sum(!is.na(data[get, "Length"])) 	
+	  		  	mat_age[ind, tmp] = sum(!is.na(data[get, "Age"]))
+	  		  }
 	  		  ind = ind + 1
 	  		}
-	  		data_sum[[place]] = mat
-	  		place = place + 1
-	  		nm = c(nm, paste0(t, "_", a))
 	  	}
-	  }
+	  	data_sum[[place]] = mat_len
+	  	data_sum[[place + 1]] = mat_age
+	  	place = place + 2
+	  	nm = c(nm,  c(paste0(a, "_length"), paste0(a, "_age")))
 	}
-	names(data_sum) = nm #paste0(sort(unique(data$Source)),"_", sort(unique(State_Area)))
+	names(data_sum) = nm 
 	summary_list$BySource_ByArea = data_sum	
 	
 	
