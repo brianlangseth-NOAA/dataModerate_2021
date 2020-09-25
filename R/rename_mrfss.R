d8b9d224f91612348d44a51a8be51b9acaf1998e
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-09-25 07:42:59 -0700

    add: add functionality to work with ODFW data

--- R/rename_mrfss.R
+++ R/rename_mrfss.R
@@ -3,33 +3,61 @@
 #' @param data read in RecFIN data
 #' @param area_grouping list of area names in data source
 #' @param area_names user area names
+#' @param len_col name of the length column to use
+#' @param len_divide value to convert between mm and cm
+#' @param column_name area field that can be used to specify catch area
 #'
 #' @return A data frame 
 #'
 #' @author Chantel Wetzel
 #' @export
 #'
-rename_mrfss <- function(data, area_grouping = NULL, area_names = NULL){
+rename_mrfss <- function(data, area_grouping = NULL, area_names = NULL, len_col = "T_LEN", len_divide = 10,
+						 column_name = "DIST"){
+
+	# Check for decimal lengths and remove
+	length <- NA
+	# Removing for now under the assumption that the conversion between fork and total is appropriate and 
+	# does not bias the data.  The practice of which length type was recorded since to change over time by
+	# state. Throwing out one or the other would remove periods of recreational data.  This decision should
+	# be revisted at some time in the future.
+	#keep <- which(data[,len_col] == floor(data[,len_col]))
+	#length[keep] <- data[keep, len_col] / len_divide
+	length <- data[,len_col] / len_divide
+
+	# Set any computed weights to NA
+	weight <- NA
+	find = which(colnames(data) %in% c("WGT", "Weight"))
+	keep <- which(data[,find] == round(data[,find], 2))
+	weight[keep] <- data[keep, find]
+
 
 	state <- ifelse( data$STATE_NAME %in% c("CALIFORNIA", "CA", "C"), "CA", 
 		     ifelse( data$STATE_NAME %in% c("OREGON", "OR", "O"), "OR",
 		     ifelse( data$STATE_NAME %in% c("WASHINGTON", "WA", "W"), "WA", "OTHER")))
 
-	sex <- matrix(NA, dim(data)[1], 1)
-	sex[data$F_SEX == 2] = "F"
-	sex[data$F_SEX == 1] = "M"
-	sex[!data$F_SEX %in% c("F", "M")] = "U"
+	sex <- NA
+	if(sum(colnames(data) %in% "F_SEX")){
+		sex[data$F_SEX == 2] = "F"
+		sex[data$F_SEX == 1] = "M"
+		sex[!data$F_SEX %in% c("F", "M")] = "U"
+	} else {
+		sex <- "U"
+	}
+
+	find = which(colnames(data) %in% c("Year", "YEAR"))
+	year = data[,find]
 	
 	if(!is.null(area_grouping)){
 		data$State_Areas = recfin_areas(data = data, 
 									    area_grouping = area_grouping, 
 										area_names = area_names,
-										column_name = "DIST")
+										column_name = column_name)
 	} else {
 		data$State_Areas = NA
 	}
 
-	data$Year = data$YEAR
+	data$Year = year
 	data$Lat = NA
 	data$Lon = NA
 	data$State  = state
@@ -37,8 +65,8 @@ rename_mrfss <- function(data, area_grouping = NULL, area_names = NULL){
 	data$Areas  = NA
 	data$Depth  = NA
 	data$Sex    = sex
-	data$Length = data$LNGTH  / 10
-	data$Weight = data$WGT
+	data$Length = length
+	data$Weight = weight
 	data$Age    = NA
 	data$Data_Type = NA
 	data$Source = "RecFIN_MRFSS"	
--- R/rename_recfin.R
+++ R/rename_recfin.R
@@ -9,36 +9,84 @@
 #' @author Chantel Wetzel
 #' @export
 #'
-rename_recfin <- function(data, area_grouping = NULL, area_names = NULL){
+rename_recfin <- function(data, area_grouping = NULL, area_names = NULL, column_name = "DIST"){
 
-	state <- ifelse( data$STATE_NAME %in% c("CALIFORNIA", "CA", "C"), "CA", 
-		     ifelse( data$STATE_NAME %in% c("OREGON", "OR", "O"), "OR",
-		     ifelse( data$STATE_NAME %in% c("WASHINGTON", "WA", "W"), "WA", "OTHER")))
+	col = which( colnames(data) %in% c("STATE_NAME", "State.Name"))
+	if (length(col) > 0){
+		state <- ifelse( data[,col] %in% c("CALIFORNIA", "CA", "C"), "CA", 
+			     ifelse( data[,col] %in% c("OREGON", "OR", "O"), "OR",
+			     ifelse( data[,col] %in% c("WASHINGTON", "WA", "W"), "WA", "OTHER")))
+	} else { 
+		stop("State name column not found. Double check file.")
+	}
 
-	sex <- ifelse(data$FISH_SEX == "F" | data$FISH_SEX == 2, "F",
-		   ifelse(data$FISH_SEX == "M" | data$FISH_SEX == 1, "M",
+	col = which( colnames(data) %in% c("FISH_SEX", "Fish.Sex") )
+	if (length(col) > 0){
+		sex <- ifelse(data[,col] == "F" | data[,col] == 2, "F",
+		   	   ifelse(data[,col] == "M" | data[,col] == 1, "M",
 		   		  "U"))
+	} else {
+		message("Sex data column not found. Double check file.")
+	}
 	
 	if(!is.null(area_grouping)){
-		data$State_Areas = recfin_areas(data = data, 
-									    area_grouping = area_grouping, 
-										area_names = area_names)
+		State_Areas = recfin_areas(data = data, 
+								   area_grouping = area_grouping, 
+								   area_names = area_names,
+								   column_name = column_name)
 	} else {
 		data$State_Areas = NA
 	}
 
-	data$Year = data$RECFIN_YEAR
+	col = which( colnames(data) %in% c("RECFIN_YEAR", "RecFIN.Year"))
+	if (length(col) > 0) {
+		year = data[,col]
+	} else {
+		stop("Year column not found. Double check file.")
+	}	
+
+	col = which( colnames(data) %in% c("RECFIN_LENGTH_MM", "RecFIN.Length.MM"))
+	if (length(col) > 0) {
+		length = data[,col] / 10
+	} else {
+		length = NA
+		message("Length data column not found. Double check file.")
+	}
+
+	col = which(colnames(data) %in% c("AGENCY_WEIGHT", "Agency.Weight"))
+	if (length(col) > 0) {
+		weight = data[,col]
+	} else {
+		weight = NA
+		message("Weight data column not found. Double check file.")
+	}
+
+	col = which(colnames(data) %in% c("IS_RETAINED", "Is.Retained"))
+	if (length(col) > 0) {
+		retain = data[,col]
+	} else {
+		retain = NA
+	}
+
+	col = which(colnames(data) %in% c("FISH_AGE", "USE_THIS_AGE"))
+	if (length(col) > 0) {
+		age = data[,col]
+	} else {
+		age = NA
+	}
+
+	data$Year = year
 	data$Lat = NA
 	data$Lon = NA
 	data$State  = state
-	data$State_Areas = data$State_Areas
+	data$State_Areas = State_Areas
 	data$Areas  = NA
 	data$Depth  = NA
 	data$Sex    = sex
-	data$Length = data$RECFIN_LENGTH_MM / 10
-	data$Weight = data$AGENCY_WEIGHT
-	data$Age    = data$FISH_AGE
-	data$Data_Type = data$IS_RETAINED
+	data$Length = length
+	data$Weight = weight
+	data$Age    = age
+	data$Data_Type = retain
 	data$Source = "RecFIN"	
 
 	return (data)
