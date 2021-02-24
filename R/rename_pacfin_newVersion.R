a1efd66abf8268f93b899b482ee52e145d97b224
brianlangseth-NOAA <brian.langseth@noaa.gov>
2021-02-24 11:00:11 -0800

    Add: new function to accomodate new PacFIN.Utilities and PacFIN data files
    Accommodate new variables, remove functionality now within cleanPacFIN

---
+++ R/rename_pacfin_newVersion.R
@@ -0,0 +1,54 @@
+#' Create standarized cleaning and renaming PacFIN data.
+#' Used with updated version of PacFIN.Utilities 0.0.1.9999
+#' as of Feb. 2021
+#'
+#' @param data read in pacfin data file
+#' @param area_grouping list of grouped area names in data source
+#' @param area_names user area names
+#' @param fleet_grouping list of grouped fleet names in data source
+#' @param fleet_names user fleet names
+#' @param fleet_column_name name of the column to group fleets by in the PacFIN data
+#'
+#' @return A data frame 
+#'
+#' @author Chantel Wetzel, Brian Langseth
+#' @export
+#'
+rename_pacfin_newVersion <- function(data, area_grouping = NULL, area_names = NULL, fleet_grouping = NULL, fleet_names = NULL, fleet_column_name = "COND"){
+
+	data <- PacFIN.Utilities::cleanPacFIN(Pdata = data, CLEAN = TRUE, verbose = TRUE)
+
+	fleets <- NA
+	if(!is.null(fleet_grouping)){
+	fleets = pacfin_fleets(data = data, 
+						   fleet_grouping = fleet_grouping, 
+						   fleet_names = fleet_names, 
+						   fleet_column_name = fleet_column_name)
+	}
+	
+	state_areas <- NA
+	if(!is.null(area_grouping)){
+		for (a in 1:length(area_grouping)){
+			get <- paste(area_grouping[[a]], collapse = "|")
+			find = grep(get, data$PCID, ignore.case = TRUE)
+			state_areas[find] = area_names[a]
+		}
+	}  
+
+	data$Year = data$SAMPLE_YEAR
+	data$Lat  = NA
+	data$Lon  = NA
+	data$State  = data$state
+	data$State_Areas  = state_areas
+	data$Areas  = NA
+	data$Depth  = NA
+	data$Sex    = data$SEX
+	data$Length = data$lengthcm
+	data$Weight = data$FISH_WEIGHT #Already in KGs
+	data$Age    = data$age
+	data$Fleet  = fleets
+	data$Data_Type = "RETAINED"
+	data$Source = "PacFIN"
+
+	return (data)
+}
